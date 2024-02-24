6bd53967b61a43e92d4b31f27ee731cafabfa6d3
Antti <antti.kiiveri@boogiesoftware.com>
2024-02-24 16:45:58 +0200

    Hakemiston siivousta

--- paaSivunSkripti.js
+++
@@ -1,6 +0,0 @@
-function aloitaTesti(tiedosto) {
-    console.log('Aloita testi:', tiedosto);
-    // Lisää haluamasi toiminnallisuus tänne
-    // Voit esimerkiksi ladata toisen sivun käyttäen window.location.href
-    // tai tehdä muita toimintoja tässä vaiheessa
-}
