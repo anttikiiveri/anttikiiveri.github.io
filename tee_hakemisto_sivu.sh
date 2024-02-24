d240ccb03d4491c1ab56b2dad1f0893ce3ab7fb7
Antti <antti.kiiveri@boogiesoftware.com>
2024-02-24 15:51:35 +0200

    WIP: Valitun tiedoston nimi välittyy halutulle funktiolle.

--- tee_hakemisto_sivu.sh
+++ tee_hakemisto_sivu.sh
@@ -24,16 +24,17 @@ for file in "$DIRECTORY"/*.csv; do
     # Otetaan vain tiedostonimi ilman hakemistopolkua
     filename=$(basename -- "$file")
     # Luodaan suhteellinen linkki HTML-sivulle ja kutsutaan aloitaTesti-funktiota
-    echo "    <li><a href=\"#\" onclick=\"aloitaTesti('$filename')\">$filename</a></li>" >> "$HTML_FILE"
+    echo "    <li><a href=\"#\" onclick=\"siirryTestiin('$filename')\">$filename</a></li>" >> "$HTML_FILE"
 done
 
 # Luodaan HTML-sivun loppuosa
 cat <<EOF >> "$HTML_FILE"
     </ul>
     <script>
-        function aloitaTesti(tiedostoNimi) {
+        function siirryTestiin(tiedostoNimi) {
             console.log('Aloita testi:', tiedostoNimi);
-            // Voit lisätä haluamasi toiminnallisuuden tässä vaiheessa
+            // Lisää tiedosto URL-parametriksi ja siirry aloitaTesti.html-sivulle
+            window.location.href = 'testi_aloita_testi.html?tiedosto=' + encodeURIComponent(tiedostoNimi);
         }
     </script>
 </body>
